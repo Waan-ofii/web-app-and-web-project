5e6dc11a51ebe7c65722bf2e32502dc223668bc0
Waan-ofii <tasfayeayele636@gmail.com>
2025-10-01 23:34:20 +0300

    header and footer of copy of apples website

--- pharmacy/scrip[t.js
+++
@@ -1,8 +0,0 @@
-const nav = document.querySelector("nav");
-window.addEventListener("scroll",()=>{
-    if(window.pageYOffset > 70){
-        nav.classList.add("nav-scroll");
-    }else{
-        nav.classList.remove("nav-scroll");
-    }
-});
