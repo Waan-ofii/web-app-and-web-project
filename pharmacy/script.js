da0de9d30f41637512d029348bab6d0166e5d684
Waan-ofii <tasfayeayele636@gmail.com>
2025-10-01 23:33:04 +0300

    adding js for  header when it scrolled

---
+++ pharmacy/script.js
@@ -0,0 +1,16 @@
+const nav = document.querySelector("nav");
+window.addEventListener("scroll",()=>{
+    if(window.pageYOffset > 70){
+        nav.classList.add("nav-scroll");
+    }else{
+        nav.classList.remove("nav-scroll");
+    }
+});
+const white=document.querySelectorAll("")
+window.addEventListener("scroll",()=>{
+    if(window.pageYOffset > 70){
+        white.classList.add("white");
+    }else{
+        white.classList.remove("white");
+    }
+});
