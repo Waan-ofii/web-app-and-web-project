d1029b4877198fca0771898f22c724227fc58349
Waan-ofii <tasfayeayele636@gmail.com>
2025-10-08 23:15:42 +0300

    to make interactive the figmaToweb

--- figmaToweb/script.js
+++ figmaToweb/script.js
@@ -1,7 +1,5 @@
 const nav = document.querySelector("nav");
-const btnTo =document.querySelector(".srvbtn");
-const serv = document.querySelector('#service--1');
-
+const navs = document.querySelector("ul");
 window.addEventListener("scroll",()=>{
     if(window.pageYOffset > 60){
         nav.classList.add("scrolled");
@@ -9,10 +7,32 @@ window.addEventListener("scroll",()=>{
         nav.classList.remove("scrolled");
     }
 });
-btnTo.addEventListener('click', function(e){
-const coords = serv.getBoundingClientRect();
 
-window.scrollTo(
- coords.left,
- coords.top);
+navs.addEventListener('click',function(e){
+const clicked = e.target.closest('.navig');
+if(!clicked) return;
+const number = clicked.dataset.into;
+const toWhere = document.querySelector(`.navig-${number}`);
+toWhere.scrollIntoView({behavior:'smooth'});
+console.log(number);
 });
+
+const allSections = document.querySelectorAll('.section');
+const revealSection = function(entries,observer){
+ const [entry] = entries;
+ if(!entry.isIntersecting) return;
+ entry.target.classList.remove('section-hidden');
+ observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+    root: null,
+    threshold: 0.15
+});
+
+allSections.forEach(function(section){
+sectionObserver.observe(section);
+section.classList.add('section-hidden');
+});
+
+
