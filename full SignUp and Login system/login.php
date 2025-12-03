4356f99635893b69408fc02825051049373cad20
Waan-ofii <tasfayeayele636@gmail.com>
2025-12-03 23:35:00 +0300

    frontend of login system

---
+++ full SignUp and Login system/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login System</title>
+    <link rel="stylesheet" href="CSS/login_style.css">
+</head>
+<body>
+    <div class="box"> 
+        <h1>Login Here</h1>
+        <form action="login.php" method="post">
+            <input type="text" name="fname" placeholder="Enter Username">
+            <input type="password" name="password" placeholder="Enter password">
+            <input type="submit" name="login" value="Login">
+            Not yet a member? <a href="signnup.php" style="color:#ffc107">SIgn Up</a>
+            
+        </form>
+    </div>
+</box>
+</body>
+</html>
